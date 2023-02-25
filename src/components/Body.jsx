e7b5c071324af5dd4cd9407a0a37fd65290a70fc
jabaww <jabaruddinsyukri@gmail.com>
2023-02-26 01:18:05 +0700

    update navbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Body = () => {
   return (
@@ -6,19 +6,11 @@ const Body = () => {
       <div className="row">
         <div className="col text-center">
           <h2 className="mb-4">Download Instagram Reels</h2>
-          <p className="mb-4">
-            Download any public Instagram post, reel, igtv, story. Paste your
-            link below and click on download button.
-          </p>
+          <p className="mb-4">Download any public Instagram post, reel, igtv, story. Paste your link below and click on download button.</p>
           <form className="row justify-content-center">
             <div className="col-auto">
               <label htmlFor="link" className="visually-hidden" />
-              <input
-                type="text"
-                className="form-control"
-                id="link"
-                placeholder="Paste link here .."
-              />
+              <input type="text" className="form-control" id="link" placeholder="Paste link here .." />
             </div>
             <div className="col-auto">
               <button type="submit" className="btn btn-secondary mb-3">
